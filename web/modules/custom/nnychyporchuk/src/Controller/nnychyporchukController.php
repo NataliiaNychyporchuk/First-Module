244996a7fc96401ddd36db09ff599909cac2152f
NataliiaNychyporchuk <natikua1996@gmail.com>
2022-07-28 12:13:42 +0300

    Creating Twig file and fixing some SCSS

--- web/modules/custom/nnychyporchuk/src/Controller/nnychyporchukController.php
+++ web/modules/custom/nnychyporchuk/src/Controller/nnychyporchukController.php
@@ -16,16 +16,16 @@ class nnychyporchukController extends ControllerBase {
 
   public function build() {
     $cats = $this->getCats();
-    $build = [
-      'content' => [
-        '#type' => 'item',
-        '#markup' => $this->t('Hello! You can add here a photo of your cat.')
-      ],
-    'form' => \Drupal::formBuilder()->getForm('\Drupal\nnychyporchuk\Form\CatsForm'),
-    'cats' => [
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('Hello! You can add here a photo of your cat.'),
+    ];
+    $build['form'] = \Drupal::formBuilder()->getForm(
+      '\Drupal\nnychyporchuk\Form\CatsForm'
+    );
+    $build['cats'] = [
       '#theme' => 'nnychyporchuk_theme_hook',
-      '#cats' => $cats,
-      ],
+      '#cats' => '$cats',
       ];
     return $build;
   }
